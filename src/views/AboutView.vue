771d2b990f29429556565800f78baa03091d0e42
Pauline Wahle <pauline.wahle@web.de>
2024-05-14 21:29:49 +0200

    about links

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,7 +17,37 @@ export default{
 
 <template>
   <div id="left-wrapper">
-
+    <div class="line"></div>
+        <div class="link-container">
+          <h2> Design </h2>
+          <div class="so-me-link">
+            <a href="https://www.behance.net/paulinewahle1" target="_blank" class="about-link"><p> Behance <p class="responsive arrow">&#8227;</p> </p></a>
+            <a href="https://dribbble.com/PaulineWahle" target="_blank" class="about-link"><p> Dribble <p class="responsive arrow">&#8227;</p> </p></a>
+            <a href="https://www.pinterest.de/PaulineWahle/" target="_blank" class="about-link"><p> Pinterest <p class="responsive arrow">&#8227;</p> </p></a>
+          </div>
+        </div>
+        <div class="line"></div>
+        <div  class="link-container">
+          <h2> Code </h2>
+          <div class="so-me-link">
+            <a target="_blank" href="https://github.com/paulinewahle" class="about-link" ><p> Github <p class="responsive arrow">&#8227;</p> </p></a>
+            <a target="_blank" href="https://codepen.io/PaulineWahle" class="about-link"><p> CodePen<p class="responsive arrow">&#8227;</p> </p></a>
+          </div>
+        </div>
+        <div @mouseover="width1 = '80%'" @mouseleave="width1 = '40%'" class="link-container">
+          <h2> Experience </h2>
+          <div class="so-me-link">
+            <a  target="_blank" href="https://www.linkedin.com/in/paulinewahle/" class="about-link"><p> LinkedIn<p class="responsive arrow">&#8227;</p> </p></a>
+          </div>
+          </div>
+          <div class="line"></div>
+        <div @mouseover="width1 = '80%'" @mouseleave="width1 = '40%'" class="link-container">
+          <h2> Recognition </h2>
+          <div class="so-me-link">
+            <a href="https://www.webguruawards.com/sites/paulinewahle-com" style="background: url(http://webguruawards.com/awards/GOTM/img_guru_of_the_month_gray.png) no-repeat; background-position: center right; background-size: contain; width:80px; height:80px; margin-top: 10%; " target="_blank" class="about-link"></a>
+          </div>
+          </div>
+      
   </div>
   <div id="right-wrapper">
     <p>Hello, I’m Pauline Wahle - user experience designer and frontend developer.
