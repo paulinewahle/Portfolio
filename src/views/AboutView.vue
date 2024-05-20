527b3e9bbfcec762aba18141aa379262718d35ed
Pauline Wahle <pauline.wahle@web.de>
2024-05-20 13:14:29 +0200

    About Text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -37,8 +37,8 @@ export default{
     </div>
   </div>
   <div id="right-wrapper">
-    <p>Hello, I’m Pauline Wahle - user experience designer and frontend developer.
-Coming from a degree in graphic design, and a following degree focusing on digital spaces with studies in user experience and front-end development, I draw from a wholistic view on design, usability and technology. Always considering the usability aspect, I find it fascinating to explore and reimagine digital spaces in new ways that surprise, simplify and create lasting impressions. By taking opportunities to establish original ideas, each design can get a unique look and feel. My basis for both design and development is creative thinking, to stay flexible, keep moving and find alternative paths.
+    <p>Hi, I’m Pauline Wahle - user experience designer and frontend developer.
+      I conceptualise and realise user friendly designs. With five years of experience and two degrees, I draw from a holistic view on design, usability and technology. Always considering the usability aspect, I find it fascinating to explore and reimagine digital spaces in new ways that surprise, simplify and create lasting impressions. By taking opportunities to establish original ideas, each design can get a unique look and feel. My basis for both design and development is creative thinking, to stay flexible, keep moving and find alternative paths.
     </p>
   </div>
 </div>
